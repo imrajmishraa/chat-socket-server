078a3cb76d04bccee11acd61e39d931202a8f315
Raj Mishra <raj212558@gmail.com>
2026-07-07 13:43:50 +0530

    feat: healthcheck api to verify connection

---
+++ src/controllers/healthz/healthz.ts
@@ -0,0 +1,12 @@
+import { asyncHandler } from "../../utils/asyncHandler";
+import { ApiResponse } from "../../utils/ApiResponse";
+
+const healthz = asyncHandler(async (req, res) => {
+  console.log("✅Healthcheck is working");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Health check passed", "OK"));
+});
+
+export { healthz };

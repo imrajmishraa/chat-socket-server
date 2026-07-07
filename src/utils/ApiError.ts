4ca3aee6063ed42702aa08cb5cdc8ef8c745cefe
Raj Mishra <raj212558@gmail.com>
2026-07-07 13:03:24 +0530

    feat: custom error handler for APIs

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,28 @@
+class ApiError extends Error {
+  public readonly statusCode: number;
+  public readonly success: boolean;
+  public readonly errors: unknown[];
+  public readonly data: null;
+
+  constructor(
+    statusCode: number,
+    message: string = "something went wrong",
+    errors: unknown[],
+    stack?: string,
+  ) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.success = false;
+    this.errors = errors;
+    this.data = null;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };

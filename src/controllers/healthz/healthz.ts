7695c636b789e5dc83c6bf16053bea68ddc6b239
Raj Mishra <raj212558@gmail.com>
2026-07-07 13:51:06 +0530

    fix: inhance API data with timestamps

--- src/controllers/healthz/healthz.ts
+++ src/controllers/healthz/healthz.ts
@@ -4,9 +4,12 @@ import { ApiResponse } from "../../utils/ApiResponse";
 const healthz = asyncHandler(async (req, res) => {
   console.log("✅Healthcheck is working");
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, "Health check passed", "OK"));
+  return res.status(200).json(
+    new ApiResponse(200, "Health check passed", {
+      status: "UP",
+      timestamp: new Date().toISOString(),
+    }),
+  );
 });
 
 export { healthz };
